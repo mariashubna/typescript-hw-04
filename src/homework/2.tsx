ef5d42a57fbcd12abb602d20237469aaf3d5d221
mariashubna <mariashubna@gmail.com>
2024-01-08 17:18:33 +0100

    ./

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,14 @@
 import React, {useReducer} from "react";
 
+
+type RequestStep = "start" | "pending" | "finished" | "idle";
+
+type State = {
+  isRequestInProgress: boolean;
+  requestStep: RequestStep;
+};
+type Action = { type: 'START_REQUEST' | 'PENDING_REQUEST' | 'FINISH_REQUEST' | 'RESET_REQUEST'};
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
